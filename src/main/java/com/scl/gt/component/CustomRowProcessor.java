6d4cd3bcc7f4499d9a29a3f0f956061d129e5dd2
vswarnkar <sonivija@gmail.com>
2022-06-29 22:36:12 +0100

    Updated var name

--- src/main/java/com/scl/gt/component/CustomRowProcessor.java
+++ src/main/java/com/scl/gt/component/CustomRowProcessor.java
@@ -1,13 +1,14 @@
 package com.scl.gt.component;
 
 import com.opencsv.processor.RowProcessor;
-import org.apache.commons.lang3.StringUtils;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
 
 public class CustomRowProcessor implements RowProcessor {
 
     @Override
     public String processColumnItem(String column) {
-        if (StringUtils.isBlank(column)) {
+        if (isBlank(column)) {
             return null;
         }
         return column.trim();
@@ -15,8 +16,8 @@ public class CustomRowProcessor implements RowProcessor {
 
     @Override
     public void processRow(String[] row) {
-        for (int i = 0; i < row.length; i++) {
-            row[i] = processColumnItem(row[i]);
+        for (int count = 0; count < row.length; count++) {
+            row[count] = processColumnItem(row[count]);
         }
     }
 
